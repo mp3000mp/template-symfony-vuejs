5107eba74dcfdd92a3fa2d6045b8bb51f2733890
mp3000mp <moussadedijon@gmail.com>
2020-05-20 00:55:17 +0200

    downgrade sf 4.4

--- config/bundles.php
+++ config/bundles.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
@@ -12,7 +12,7 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php
 
 use App\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
