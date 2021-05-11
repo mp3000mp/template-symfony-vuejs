d13ca075d20dd432531d0249137ec54a1f5f1b1d
Matthias Perret <moussadedijon@gmail.com>
2021-05-12 01:53:07 +0200

    codecov flags

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   transform: {
     '^.+\\.vue$': 'vue-jest'
   },
-  collectCoverageFrom: ['src/**/*.{js,ts,vue}', '!src/router/*.*', '!src/*.{js,ts,vue}'],
+  collectCoverageFrom: ['src/**/*.{js,ts,vue}'],
+  coveragePathIgnorePatterns: ['tests', 'src/router/*.*', 'src/*.{js,ts,vue}'],
   coverageDirectory: './ci/coverage/unit'
 }
