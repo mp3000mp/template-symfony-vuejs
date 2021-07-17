20c461ae11ff81e2fbb7b11cc789d3f7e53e57cc
Matthias Perret <moussadedijon@gmail.com>
2021-07-17 15:47:13 +0200

    tests user create command

---
+++ backend/tests/Functional/Command/AbstractCommandTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Tests\Functional\Command;
+
+use App\DataFixtures\AppTestFixtures;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\DataFixtures\ContainerAwareLoader;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+abstract class AbstractCommandTest extends KernelTestCase
+{
+    protected EntityManagerInterface $em;
+
+    protected Application $application;
+
+    protected function setUp(): void
+    {
+        $kernel = self::bootKernel();
+        $this->application = new Application($kernel);
+
+        // utils
+        $this->em = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        // reset database
+        $purger = new ORMPurger($this->em, []);
+        $purger->setPurgeMode(ORMPurger::PURGE_MODE_DELETE);
+        $loader = new ContainerAwareLoader(self::$kernel->getContainer());
+        $loader->addFixture(new AppTestFixtures());
+        $executor = new ORMExecutor($this->em, $purger);
+        $executor->execute($loader->getFixtures());
+
+        // parent
+        parent::setUp();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->em->close();
+        unset($this->em);
+    }
+
+    protected function getCommandTester(string $commandName): CommandTester
+    {
+        $command = $this->application->find($commandName);
+
+        return new CommandTester($command);
+    }
+}
---
+++ backend/tests/Functional/Command/CreateUserCommandTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Tests\Functional\Command;
+
+use App\Entity\User;
+
+class CreateUserCommandTest extends AbstractCommandTest
+{
+    /**
+     * @dataProvider providerExecute
+     */
+    public function testExecute(bool $isAdmin, array $expectedRoles): void
+    {
+        $commandName = 'app:user:create';
+        $commandTester = $this->getCommandTester($commandName);
+
+        $commandTester->execute([
+            'command' => $commandName,
+            'username' => 'test',
+            'email' => 'test@mp3000.fr',
+            'password' => 'Test2000!',
+            '--is-admin' => $isAdmin,
+        ]);
+
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('SUCCESS', $output);
+
+        preg_match('/\(id=(\d+)\)/', $output, $arr);
+        $id = (int) $arr[1];
+
+        $user = $this->em->getRepository(User::class)->find($id);
+        $this->assertEquals($expectedRoles, $user->getRoles());
+    }
+
+    public function providerExecute(): array
+    {
+        return [
+            'Is not admin' => [
+                'isAdmin' => false,
+                'expectedRoles' => ['ROLE_USER'],
+            ],
+            'Is admin' => [
+                'isAdmin' => true,
+                'expectedRoles' => ['ROLE_USER', 'ROLE_ADMIN'],
+            ],
+        ];
+    }
+}
